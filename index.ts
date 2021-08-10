8f1145c854287e91fe46bd0c9cfefe2d059b66ca
dg12988 <doug.goldberg@me.com>
2021-08-10 01:30:56 -0400

    1 file modified

--- index.ts
+++ index.ts
@@ -1,13 +1,13 @@
 import { Observable } from "rxjs";
 
-import { Observable } from 'rxjs'
-
 const helloButton = document.querySelector('button#hello');
 
-const helloClick$ = new Observable(subscriber => {
+const helloClick$ = new Observable<MouseEvent>(subscriber => {
   helloButton.addEventListener('click', (event) => {
     subscriber.next(event);
   });
 });
 
-helloClick$.subscribe(event => console.log(event));
+helloClick$.subscribe(event => console.log('Sub 1:', event.type, event.x, event.y));
+
+helloClick$.subscribe(event => console.log('Sub 2:', event.type, event.x, event.y));
